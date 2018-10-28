a9200d9345d36f5f727a520a5fc80c769b923729
SokolX <89soek@gmail.com>
2018-10-28 09:56:52 +0100

    added user address dao and user address dao impl

---
+++ src/main/java/pl/sokolx/api/UserAddressDao.java
@@ -0,0 +1,13 @@
+package pl.sokolx.api;
+
+import pl.sokolx.models.UserRole;
+
+import java.util.List;
+
+public interface UserRoleDao {
+
+    public List<UserRole> getAllUserRoles();
+    public UserRole getUserRoleById(Integer roleId);
+    public Integer getUserRoleIdByName(String roleName);
+
+}
